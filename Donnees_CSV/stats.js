e3ad9a2ef2c8f4050ee40c3261a8236f375c695e
Layer09 <119010896+Layer09@users.noreply.github.com>
2025-03-23 17:46:25 +0100

    Update stats.js

--- Donnees_CSV/stats.js
+++ Donnees_CSV/stats.js
@@ -177,8 +177,8 @@ function createTable(data) {
                 const img = document.createElement('img');
                 img.src = "https://upload.wikimedia.org/wikipedia/commons/4/42/YouTube_icon_%282013-2017%29.png"; // URL du logo YouTube
                 img.alt = "YouTube";
-                img.style.width = "24px"; // Taille du logo (ajustable)
-                img.style.height = "24px"; // Taille du logo (ajustable)
+                img.style.width = "21px"; // Taille du logo (ajustable)
+                img.style.height = "15px"; // Taille du logo (ajustable)
                 img.style.cursor = "pointer"; // Changer le curseur pour une meilleure expérience utilisateur
                 a.appendChild(img); // Ajouter l'image au lien
                 td.appendChild(a); // Ajouter le lien (contenant l'image) à la cellule
