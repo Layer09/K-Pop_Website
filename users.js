f46b23a1f43f5e2d1e87488a1832ec4ba2564969
Layer09 <119010896+Layer09@users.noreply.github.com>
2025-03-29 01:06:27 +0100

    Create users.js

---
+++ users.js
@@ -0,0 +1,7 @@
+const users = [
+    { username: "anna", password: "ae5f4cceb2b7c4eaac9374684dbe2a68c087995fc3bc4b3308504beeb6d484b9" }, // 12032004
+    { username: "melyssa", password: "6cd780750f65844a2193cb590e399abf8e7b6e3df147df6b68a2cb83f35b08db" }, // 07012004
+    { username: "laurana", password: "4fdb1521906e0f2e7c85b2ff1fc14449a14db5dd5608cb5de2d1c2fb9a05c3a5" }, // 09072001
+    { username: "jeremy", password: "309cf8ea909b6ef1b4cb308dddfb8a6de64c7a2ed3b2e034abafce7492c54f48" }, // 12091996
+    { username: "andy", password: "48f4f34ff3c3eb4e47418706068fa8bc264f7ad6df4c41584ffec43cc0cc1c98" }  // 04121997
+];
