69476a227fa18df32c5436df8353e3c181761cd4
Layer09 <119010896+Layer09@users.noreply.github.com>
2025-04-04 17:42:54 +0200

    Update users.js

--- users.js
+++ users.js
@@ -2,6 +2,5 @@ const users = [
     { username: "anna", password: "d67b32d66a2648b39cdb57074232804fcbf6d43a04fbd7e66b0468ab0c3f9ad7" },
     { username: "melyssa", password: "a7361d1aafdf672f96a6f37566f5958aa7747407093f92c5d79632bb67d557ca" },
     { username: "laurana", password: "847a30b29119055aa53ec6a26a8e3df64ddb325feb2b2e8989f0bf263194781d" },
-    { username: "jeremy", password: "332418375d0118e50a3d928416efd13b3d35cd69f6b99d46f305d9c08874d806" },
     { username: "andy", password: "9031c61216ba5ac28163dfc3a650e02641b71af3d55e1a59eaa2a2c5e0b4b21f" }
 ];
